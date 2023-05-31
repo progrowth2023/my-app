39453ec14248638979c5f9e214779c3e22534591
ProGrowth Web Developers <123374278+progrowth2023@users.noreply.github.com>
2023-05-31 13:02:55 +0530

    Update page.js
    Mobile checking

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
       <div className="bg-gradient-to-br to-[#243c5a] from-teal-800">
       <div className="max-w-screen-xl mx-auto p-[4rem] -mt-[4.5rem] ">
         <div className="flex flex-wrap justify-around items-center">
-          <div>Left</div>
-          <div>Right</div>
+          <div>Left Side</div>
+          <div>Right Side</div>
         </div>
       </div>
       </div>
